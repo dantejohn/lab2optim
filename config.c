1266ccca2b8939da2f849e36ad7ffe7af8d143c4
Eugene Dementyev <edementyev@plgn.ru>
2015-11-27 15:46:44 +0500

    It doesn't work but need to becommited because

--- config.c
+++ config.c
@@ -18,9 +18,7 @@
 double a = 2, b = -1.3, c = 0.02, d = 0.14;
 double e[5] = {0.1, 0.01, 0.001, 0.0001, 0.00001};
 int N0, N1, N2, N, k, i;
-double x[3000][2], y[10];
-double z1, z2;
-double A, B;
+
 
 
 void config (void);
@@ -64,12 +62,12 @@ dz2dx1x2 (double x1, double x2) {
 
 int
 main() {
-  for (i=0; i < 5; i++) {
+  for (i=1; i < 2; i++) {
   printf("Iteration %d with e = %lf\n", i+1, e[i]);
-  grdopt();
+  gradopt();
   }
 }
-
+/*
 void
 config () {
   double x1min, x2min, ymin;
@@ -133,10 +131,64 @@ config () {
   N = N0 + N1 + N2;
   printf("%f; %f; %f; %d; %d\n", x1min, x2min, ymin, N, k);
 }
-
+*/
 void
 gradopt() {
-  int k, l1, l2;
+  int k, m;
+  double o, q, s, ymin, x1min, x2min, e0, alpha, y1, y2;
+  double z1, z2, p, alpmin, g1, g2;
   double x[3000][2]; double y[10];
-  x[0][1] = 
+  x[0][0] = -1; x[0][1] = 0;
+  k = 0; N0 = 0; N1 = 0; N2 = 0; N = 0; alpha = 2.2;
+  z1 = dzdx1 (x[0][0], x[0][1]); z2 = dzdx2 (x[0][0], x[0][1]);
+mm1:
+  m = 0;
+  y1 = dz (x[k][0],x[k][1]);
+metka:
+  y2 = dz (x[k][1]-(m+1)*alpha*z1,x[k][1]-(m+1)*alpha*z2);
+  if (y2 < y1) {
+    m++; y1=y2; goto metka;
+  }
+  else {
+    q = (m+1)*alpha;
+    if (m == 0) {
+      o = 0;
+    }
+    else {
+      o = (m-1)*alpha;
+    }
+  }
+  do {
+    p = (o+q)/2;
+    g1 = dz (x[k][0]-(p-0.0001)*z1,x[k][1]-(p-0.0001)*z2);
+    g2 = dz (x[k][0]-(p+0.0001)*z1,x[k][1]-(p+0.0001)*z2);
+    if (g1 < g2) {
+      q = p + 0.0001;
+    }
+    else {
+      o = p - 0.0001;
+    }
+  } while ((q-o)/2>2*e[i]);
+  alpmin = (o+q)/2;
+  printf("\nk=%d", k+1 );
+  x[k+1][0]=x[k][0]-alpmin*z1; 
+  x[k+1][1]=x[k][1]-alpmin*z2; 
+  if (i==1) {
+    printf("\nx[0][0]= %lf", x[k+1][0]);
+    printf("\nx[0][1]= %lf", x[k+1][1]);
+  }
+  z1=dzdx1(x[k+1][0],x[k+1][1]);
+  z2=dzdx2(x[k+1][0],x[k+1][1]);
+  q = pow (z1*z1+z2*z2, 0.5);
+  if (d > 2.5*e[i]) {
+    k++; goto mm1;
+  }
+  else {
+    x1min = x[k+1][0];
+    x2min = x[k+1][1];
+    ymin = dz (x1min, x2min);
+    N = N0 + N1 -1;
+    printf ("\n%f; %f; %f; %d; %d; %d; k = %d;\n", x1min, x2min, ymin, N0-1, N1, N, k+1);
+  }
+
 }
