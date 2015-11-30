72036ca3db68404a00ce54875f5920f026d99d11
Eugene Dementyev <edementyev@plgn.ru>
2015-11-30 14:30:00 +0500

    Newton work

--- newton.c
+++ newton.c
@@ -26,47 +26,56 @@ int N0, N1, N2, N, k, i;
 
 
 double
-df (double x1, double x2) {
+df (double x1, double x2) 
+{
   N0++;
   return Aa * x1 + Bb * x2 + exp(C * pow(x1, 2) + D * pow(x2, 2));
 }
 
 double
-dfx1 (double x1, double x2) {
+dfx1 (double x1, double x2) 
+{
   N1++;
   return Aa + 2 * C * x1 * exp(C * pow(x1, 2) + D * pow(x2, 2));
 }
 
 double
-dfx2 (double x1, double x2) {
+dfx2 (double x1, double x2) 
+{
   N1++;
   return Bb + 2 * D * x2 * exp(C * pow(x1, 2) + D * pow(x2, 2));
 }
 
 double
-dfx1x1 (double x1, double x2) {
+dfx1x1 (double x1, double x2) 
+{
   N2++;
   return (pow(2 * C, 2) * pow(x1, 2) + 2 * C) * exp(C * pow(x1, 2) + D * pow(x2, 2));
 }
 
 double
-dfx2x2 (double x1, double x2) {
+dfx2x2 (double x1, double x2) 
+{
   N2++;
   return (pow(2 * D, 2) * pow(x2, 2) + 2 * D) * exp(C * pow(x1, 2) + D * pow(x2, 2));
 }
 
 double
-dfx1x2 (double x1, double x2) {
+dfx1x2 (double x1, double x2) 
+{
   N2++;
   return 4 * C * D * x1 * x2 * exp(C * pow(x1, 2) + D * pow(x2, 2));
 }
 
-double fi(int ki,double dalf,double x1,double x2,double z)
-
-{return df(x1-dalf*z,x2);}
+double 
+fi(int ki,double dalf,double x1,double x2,double z)
+{
+  return df (x1-dalf*z,x2);
+}
 
 void
-newtonopt() {
+newtonopt() 
+{
   double x[1000][2];
   double y[2][2];
   k=0, N1 = 0; N2 = 0; N0 = 0;
@@ -81,48 +90,58 @@ newtonopt() {
   y2 = dfx2 (x[0][0], x[0][1]);
 
   p1:
-    y[0][0] = dfx1x1(x[k][0], x[k][1]);
-    y[0][1]=dfx1x2(x[k][0],x[k][1]);
-    y[1][0]=y[0][1];
-    y[1][1]=dfx2x2(x[k][0],x[k][1]);
-    det = y[0][0]*y[1][1]-y[0][1]*y[1][0];
-    p1=(y1*y[1][1]-y2*y[0][1])/det;
-    p2=(y[0][0]*y2-y[1][0]*y1)/det;
-    m=0;
-    ym1 = df(x[k][0], x[k][1]);
+    y[0][0] = dfx1x1 (x[k][0], x[k][1]);
+    y[0][1] = dfx1x2 (x[k][0], x[k][1]);
+    y[1][0] = y[0][1];
+    y[1][1] = dfx2x2 (x[k][0], x[k][1]);
+    det = y[0][0] * y[1][1] - y[0][1] * y[1][0];
+    p1 = (y1 * y[1][1] - y2 * y[0][1]) / det;
+    p2 = (y[0][0] * y2 - y[1][0] * y1) / det;
+    m = 0;
+    ym1 = df (x[k][0], x[k][1]);
+
   ab:
-    ym2 = fi(k, dalf*(m+1), x[k][0], x[k][1], y1);
-    if (ym2<ym1)
-{m++;ym1=ym2;goto ab;}
-else
-{ b1=(m+1)*dalf;
-if (m==0) a1=0; else a1=(m-1)*dalf;}
-ma=a1+(b1-a1)/n;
-min=df(x[k][0]-ma*y1,x[k][1]-ma*y2);N1++;
-l=1;
-for (j=2;j<=n;j++)
-{ ma=a1+j*(b1-a1)/n;
-kan=df(x[k][0]-ma*y1,x[k][1]-ma*y2);N1++;
-if (min>kan) {min=kan;l=i;}
+    ym2 = fi (k, dalf * (m + 1), x[k][0], x[k][1], y1);
+    if (ym2 < ym1) {
+      m++;
+      ym1 = ym2;
+      goto ab;
+    }
+    else { 
+      b1 = (m + 1) * dalf;
+      if (m == 0) {
+        a1=0;
+      } 
+      else { 
+        a1 = (m - 1) * dalf;
+      }
+    }
+    ma = a1 + (b1 - a1) / n;
+    min = df (x[k][0] - ma * y1, x[k][1] - ma * y2);
+    l = 1;
+    for (j = 2; j <= n; j++){ 
+      ma = a1 + j * (b1 - a1) / n;
+      kan = df (x[k][0] - ma * y1, x[k][1] - ma * y2);
+      if (min > kan) {
+        min = kan;
+        l = i;
+      }
+    }
+    x[k + 1][0] = x[k][0] - (l * (b1 - a1) / n) * p1;
+    x[k + 1][1] = x[k][1] - (l * (b1 - a1) / n) * p2;
+    y1 = dfx1 (x[k][0], x[k][1]);
+    y2 = dfx2 (x[k][0], x[k][1]);
+    N0 -= 2;
+    del = pow (y1 * y1 + y2 * y2, 0.5);
+    k++;
+    if (del > e[i]) {
+      goto p1;
+    }
+    else {
+      printf ("x1 = %lf\n x2 = %lf\n y = %lf\n N0 = %d\n N1 = %d\n N2 = %d\n N = %d\n k = %d\n",
+              x[k][0], x[k][1], df(x[k][0], x[k][1]), N0-1, N1, N2, N-1, k);
+    }
 }
-x[k+1][0]=x[k][0]-(l*(b1-a1)/n)*p1;
-
-x[k+1][1]=x[k][1]-(l*(b1-a1)/n)*p2;
-
-y1=dfx1(x[k][0],x[k][1]);
-y2=dfx2(x[k][1],x[k][2]);
-N0-=2;
-del=pow(y1*y1+y2*y2,0.5);
-k++;
-if (del > e[i]) goto p1;
-else
-{
-  printf ("x1 = %lf\n x2 = %lf\n y = %lf\n N0 = %d\n N1 = %d\n N2 = %d\n N = %d\n k = %d\n",
-          x[k][0], x[k][1], df(x[k][0], x[k][1]), N0-1, N1, N2, N-1, k);
-
-}
-}
-
 
 int
 main() {
